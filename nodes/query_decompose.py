ad9790674c6d4b7f5fd63de586dffd59e55e6703
Soham Datta <dattasoham05@gmail.com>
2026-07-07 02:18:43 +0530

    successfully implemented resolve company ticker

--- nodes/query_decompose.py
+++ nodes/query_decompose.py
@@ -1,12 +1,13 @@
 from graph.state import GraphState
 from config.llm_gateway import querydecomposer
+from tools.resolve_c import resolve_company
 
 
 def QueryDecompose(state: GraphState):
     """This node takes a user query and extracts entity infomration"""
     output = querydecomposer(state['query'])
     return {
-        "company": output.company,
+        "company": resolve_company(output.company),
         "start_date": output.start_date,
         "end_date": output.end_date,
         "intent": output.intent
